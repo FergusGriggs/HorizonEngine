8dd1c46ab2c395f1f7be9a2aa5a986c36f641e17
Fergus Griggs <FergusGriggs@users.noreply.github.com>
2022-01-01 01:48:53 +0000

    Improved depth of field effect

--- HorizonEngine/res/shader/pixel/misc/ps_depth_of_field.hlsl
+++ HorizonEngine/res/shader/pixel/misc/ps_depth_of_field.hlsl
@@ -7,7 +7,7 @@ Texture2D normalTexture : TEXTURE : register(t0);
 Texture2D blurredTexture : TEXTURE : register(t1);
 Texture2D depthTexture : TEXTURE : register(t2);
 
-static const float sc_focusDepth = 0.3f;
+static const float sc_focusDepth = 0.1f;
 
 float getLinearProgress(float edge0, float edge1, float x)
 {
@@ -16,11 +16,16 @@ float getLinearProgress(float edge0, float edge1, float x)
 
 float4 main(VSPS_TRANSFER input) : SV_TARGET
 {
+    float focusDepth = pow(abs(depthTexture.Sample(samplerState, float2(0.5f, 0.5f)).r), 50.0f);
+    
     float3 normalColour = normalTexture.Sample(samplerState, input.texCoord).rgb;
     float3 blurredColour = blurredTexture.Sample(samplerState, input.texCoord).rgb;
-    float depth = pow(abs(depthTexture.Sample(samplerState, input.texCoord).r), 40.0f);
+    float depth = pow(abs(depthTexture.Sample(samplerState, input.texCoord).r), 50.0f);
     
-    float depthDiff = pow(getLinearProgress(0.1f, 0.25f, abs(sc_focusDepth - depth)), 2.0f);
+    float startTransition = min(1.0f, 0.1f);
+    float endTransition = min(1.0f, 0.25f + focusDepth * 0.5f);
+    
+    float depthDiff = pow(getLinearProgress(startTransition, endTransition, abs(focusDepth - depth)), 2.0f);
     
     float3 finalColour = lerp(normalColour, blurredColour, depthDiff);
     
